1b25fe33d21e05af80272da2be6965e18ed8ef1a
h-miiaah <haim.miiaah5@gmail.com>
2021-12-31 15:01:51 -0500

    if conditional

--- SearchInsertPositionSolution.js
+++ SearchInsertPositionSolution.js
@@ -5,6 +5,8 @@
  */
  var searchInsert = function(nums, target) {
     for (let position = 0; position < nums.length; position++) {
-        
+        if (target <= nums[position]) {
+            return position;
+          }
       }
 };
